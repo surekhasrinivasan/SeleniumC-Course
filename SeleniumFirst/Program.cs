30cc7df39ab7d3cf93513623bc39d0c4369400b3
surekhasrinivasan <surekhasrao@gmail.com>
2018-07-17 06:05:56 -0400

    Initial commit

--- SeleniumFirst/Program.cs
+++ SeleniumFirst/Program.cs
@@ -3,13 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace SeleniumFirst
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) 
         {
+            //create the reference for our browser
+            IWebDriver driver = new ChromeDriver();
+
+            //Navigate to Google page
+            driver.Navigate().GoToUrl("https://www.google.com");
+
+            //Find the Element
+            IWebElement element = driver.FindElement(By.Name("q"));
+
+            //Perform operation
+            element.SendKeys("executeautomation");
+
+            //close the google web browser
+            driver.Close();
         }
     }
 }
