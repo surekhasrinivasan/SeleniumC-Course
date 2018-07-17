9cea93b1f03b9afdf41428a4267cfb02274c7f74
surekhasrinivasan <surekhasrao@gmail.com>
2018-07-17 07:19:07 -0400

    Writing Test method using NUnit in Selenium C#

--- SeleniumFirst/Program.cs
+++ SeleniumFirst/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 
@@ -10,22 +11,46 @@ namespace SeleniumFirst
 {
     class Program
     {
+        //create the reference for our browser
+        IWebDriver driver = new ChromeDriver();
+
         static void Main(string[] args) 
         {
-            //create the reference for our browser
-            IWebDriver driver = new ChromeDriver();
 
+        }
+
+        [SetUp]
+        public void Initialize()
+        {
             //Navigate to Google page
             driver.Navigate().GoToUrl("https://www.google.com");
+            Console.WriteLine("Opened URL");
 
+        }
+
+        [Test]
+        public void ExecuteTest()
+        {
             //Find the Element
             IWebElement element = driver.FindElement(By.Name("q"));
 
             //Perform operation
             element.SendKeys("executeautomation");
+            Console.WriteLine("Executed Test");
+        }
 
+        [Test]
+        public void NextTest()
+        {
+            Console.WriteLine("Next Method");
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
             //close the google web browser
             driver.Close();
+            Console.WriteLine("Closed the browser");
         }
     }
 }
