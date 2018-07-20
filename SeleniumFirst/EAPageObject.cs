c4a39b712ac61bef971305c0a9ce47de585c6df4
surekhasrinivasan <surekhasrao@gmail.com>
2018-07-20 06:43:23 -0400

    Modified EAPageObject file to perform Page Navigation of POM

--- SeleniumFirst/EAPageObject.cs
+++ SeleniumFirst/EAPageObject.cs
@@ -23,7 +23,21 @@ namespace SeleniumFirst
         [FindsBy(How=How.Name, Using = "Initial")]
         public IWebElement txtInitial { get; set; }
 
+        [FindsBy(How = How.Name, Using ="FirstName")]
+        public IWebElement txtFirstName { get; set; }
+
+        [FindsBy(How = How.Name, Using ="MiddleName")]
+        public IWebElement txtMiddleName { get; set; }
+
         [FindsBy(How =How.Name, Using = "Save")]
         public IWebElement btnSave { get; set; }
+
+        public void FillUserForm(string initial, string firstName, string middleName)
+        {
+            txtInitial.SendKeys(initial);
+            txtFirstName.SendKeys(firstName);
+            txtMiddleName.SendKeys(middleName);
+            btnSave.Click();
+        }
     }
 }
